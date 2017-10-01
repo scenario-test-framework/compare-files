2fafda5f9942a8b0989c94f007abfe04be4a97c3
Shinichi Suwa <suwash01@gmail.com>
2017-10-01 12:11:38 +0900

    sonar解析を追加

--- build/build-production.sh
+++ build/build-production.sh
@@ -7,19 +7,14 @@ cd $(cd $(dirname $0); cd ..; pwd;)
 # --------------------------------------------------
 # 設定
 # --------------------------------------------------
-readonly NAME_PROJ="compare-files"
+readonly NAME_PROJ="$(basename $(pwd))"
 readonly DIR_DEPLOY="./deploy"
 readonly DIR_DOCS="./docs"
 readonly PROFILE="production"
 
-# package & site
-readonly CMD_BUILD="mvn clean package site:site -P ${PROFILE} -DPID=$$"
-
-# package only
-# readonly CMD_BUILD="mvn clean package -P ${PROFILE} -PID=$$"
-
-# package only & skip test
-# readonly CMD_BUILD="mvn clean package -Dmaven.test.skip=true -P ${PROFILE}"
+readonly SONAR_URL="https://sonarcloud.io"
+readonly SONAR_ORGANIZATION="suwa-sh-github"
+readonly SONAR_EXCLUDES="src/test/**,**/classification/**,**/dto/**,**/exception/**,**/*Const.java"
 
 # --------------------------------------------------
 # build
@@ -37,8 +32,22 @@ echo
 echo --------------------------------------------------
 echo  build
 echo --------------------------------------------------
-echo ${CMD_BUILD}
-${CMD_BUILD}
+if [[ "${SONAR_TOKEN}x" = "x" ]]; then
+  echo "SONAR_TOKEN が定義されていません。sonar解析をスキップします。"
+  readonly CMD_BUILD="mvn clean package site:site -P ${PROFILE} -DPID=$$"
+  echo ${CMD_BUILD}
+  ${CMD_BUILD}
+
+else
+  readonly CMD_BUILD="mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent package site:site sonar:sonar -P ${PROFILE} -DPID=$$"
+  echo ${CMD_BUILD}
+  ${CMD_BUILD}                                                                                     \
+    -Dsonar.host.url=${SONAR_URL}                                                                  \
+    -Dsonar.organization=${SONAR_ORGANIZATION}                                                     \
+    -Dsonar.login=${SONAR_TOKEN}                                                                   \
+    -Dsonar.exclusions="${SONAR_EXCLUDES}" 2>&1 | tee -a ${PATH_LOG} 2>&1
+fi
+
 ret_code=$?
 if [ ${ret_code} -ne 0 ]; then
     echo "maven buildでエラーが発生しました。" >&2
@@ -68,7 +77,6 @@ xargs -I{} bash -c 'cat {} | sed -e "s|\.resources/|resources/|g" > {}.tmp'
 find ${DIR_DOCS}/site/jacoco -type f -name '*.html.tmp'                           |
 xargs -I{} bash -c 'filename={}; mv -f ${filename} ${filename%.*}'
 mv ${DIR_DOCS}/site/jacoco/.resources/ ${DIR_DOCS}/site/jacoco/resources/
-rm -f ${DIR_DOCS}/site/jacoco/{}.tmp
 
 # 結果表示
 echo "${DIR_DOCS}/site"
